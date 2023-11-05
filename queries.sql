f4d3dd6ec4d2ce99d4f2828a9bba56e2687ea5a6
maggiemarks1 <maggiemarkus1@gmail.com>
2023-11-05 15:26:36 -0600

    draft

--- queries.sql
+++ queries.sql
@@ -7,4 +7,8 @@ WHERE location = "St. Louis City";
 --Part 3
 DROP TABLE job;
 --Part 4
-DROP TABLE job;
+
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;"
